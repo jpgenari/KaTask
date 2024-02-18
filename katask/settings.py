73324e8d77191abd7cf5b0eb271d86c39f3ba1d8
Joao Paulo Genari <jpgenari@gmail.com>
2024-02-18 21:44:21 +0000

    Feat: create app models and migrate to Database

--- katask/settings.py
+++ katask/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7h!sb3^48v2@cl^izip6o*c!a5vn!l)w$w6&%tqhij%-*ynuy)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'tasks'
 ]
 
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Task(models.Model):
+    """
+    Stores a single task entry related to :model:`auth:User`
+    """
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    category = models.CharField(max_length=100, blank=True, null=True)
+    due_date = models.DateField(blank=True, null=True)
+    priority = models.IntegerField(choices=((1, 'Low'), (2, 'Medium'), (3, 'High')), default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['due_date', 'created_at']
+    
+    def __str__(self):
+        return self.title
