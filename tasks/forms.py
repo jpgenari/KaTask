9f85f0f39c3b23218615758394aae1b8d8325543
Joao Paulo Genari <jpgenari@gmail.com>
2024-02-24 17:14:10 +0000

    Feat: create and code to tasks/forms.py

---
+++ tasks/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Task
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = [
+            'title',
+            'description',
+            'featured_image',
+            'category',
+            'due_date',
+            'priority',
+            'completed'
+            ]
