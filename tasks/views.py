61e28ca9a321c45a8b22f2b8284ccb356f3847cf
Joao Paulo Genari <jpgenari@gmail.com>
2024-02-24 23:42:48 +0000

    Feat: add complete and undo complete buttons from task list view

--- tasks/views.py
+++ tasks/views.py
@@ -70,6 +70,18 @@ def edit_task(request, task_id):
         {'form': form}
         )
 
+def complete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.completed = True
+    task.save()
+    return redirect('task_list')
+
+def undo_complete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.completed = False
+    task.save()
+    return redirect('task_list')
+
 def delete_task(request, task_id):
     '''
     Handles deletion of existing task.
