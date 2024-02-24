73a0c7b9c78869c1e11b7ce3224c5e8da171c434
Joao Paulo Genari <jpgenari@gmail.com>
2024-02-24 17:47:40 +0000

    Feat: add SummerNote to admin panel

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Task
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Task)
+class TasksAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'description', 'priority', 'completed', 'user')
+    search_fields = ['title', 'description', 'user']
+    list_filter = ('description', 'priority', 'completed', 'user')
+    prepopulated_fields = {'priority': ('title',)}
+    summernote_fields = ('title',)
 
 
 # Register your models here.
-admin.site.register(Task)
