3fff6fc1fe063c43b41a550876840523f1425552
Joao Paulo Genari <jpgenari@gmail.com>
2024-02-24 20:19:20 +0000

    Fix: flush database and migrate updated version

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('create/', views.create_task, name='create_task'),
+    path('create_category/', views.create_category, name='create_category'),
     path('edit/<int:task_id>/', views.edit_task, name='edit_task'),
+    path('edit/<int:category_id>/', views.edit_category, name='edit_category'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
+    path('delete/<int:category_id>/', views.delete_category, name='delete_category'),
 ]
