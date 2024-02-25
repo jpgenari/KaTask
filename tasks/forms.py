a047f6f5ca7ed94ace112c133ef02ee5d9ce04e9
Joao Paulo Genari <jpgenari@gmail.com>
2024-02-25 01:25:37 +0000

    Feat: add code to force accept only images to upload

--- tasks/forms.py
+++ tasks/forms.py
@@ -16,8 +16,20 @@ class TaskForm(forms.ModelForm):
         widgets = {
             'due_date': forms.DateInput(
                 attrs={'type': 'date', 'class': 'form-control'}
-            )
+            ),
+            'featured_image': forms.FileInput(attrs={'accept': 'image/*'}),
         }
+
+    def clean_featured_image(self):
+        featured_image = self.cleaned_data.get('featured_image')
+
+        if featured_image:
+            # Check if the uploaded file is an image
+            content_type = featured_image.content_type.split('/')[0]
+            if content_type not in ['image']:
+                raise forms.ValidationError('File type is not supported. Please upload an image.')
+
+        return featured_image
         
 class CategoryForm(forms.ModelForm):
     class Meta:
