69e665e02bf79e9a157d5d63c965be155ff5696a
Joao Paulo Genari <jpgenari@gmail.com>
2024-03-04 18:51:51 +0000

    Fix: run command to update static files

---
+++ staticfiles/js/delete.js
@@ -0,0 +1,29 @@
+const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+const deleteButtons = document.getElementsByClassName("btn-delete");
+const deleteConfirm = document.getElementById("deleteConfirm");
+
+/**
+ * Initializes deletion functionality for the provided delete buttons.
+ *
+ * For each button in the `deleteButtons` collection:
+ * - Retrieves the associated task's ID upon click.
+ * - Updates the `deleteConfirm` link's href to point to the
+ * deletion endpoint for the specific task.
+ * - Displays a confirmation modal (`deleteModal`) to prompt
+ * the user for confirmation before deletion.
+ */
+for (let button of deleteButtons) {
+    button.addEventListener("click", (e) => {
+        let taskId = e.target.getAttribute("data-task-id"); // Use data attribute to store task ID
+        deleteConfirm.href = `/delete-task/${taskId}/`; // Update with the correct URL for deleting a task
+        deleteModal.show();
+    });
+}
+
+for (let button of deleteButtons) {
+    button.addEventListener("click", (e) => {
+        let categoryId = e.target.getAttribute("data-category-id"); // Use data attribute to store task ID
+        deleteConfirm.href = `/delete-category/${categoryId}/`; // Update with the correct URL for deleting a task
+        deleteModal.show();
+    });
+}
